4b7c71ac5a69053c8899bb0bcd4e3fe58c65c729
rdematos <rdematos@IDFPLD93.polyexpert.nat>
2016-01-25 09:12:47 +0100

    Préparation des classes pour jUnit

---
+++ src/tests/testPersonne.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author rdematos
+ *
+ */
+public class testPersonne {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
