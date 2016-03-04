49576164bf8756fb887715627ff763b9f52021a3
kokojack <kokojackeve@gmail.com>
2016-03-04 01:50:24 +0100

    Début de l'implémentation de l'IHM 	--> création du package ihm 	--> création de la classe FenetrePrincipale 	--> création du jFrame Accueil

---
+++ src/ihm/FenetrePrincipale.java
@@ -0,0 +1,31 @@
+package ihm;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class FenetrePrincipale extends JFrame {
+	public FenetrePrincipale()
+	{
+		super("INSCRIPTIONS SPORTIVES PAR M2L");
+		
+		WindowListener l = new WindowAdapter()
+		{
+
+			public void windowClosing(WindowEvent e)
+			{
+				System.exit(0);
+			}
+		};
+
+			addWindowListener(l);
+			setSize(200,100);
+			setVisible(true);
+	}
+	
+	public static void main(String [] args){
+		JFrame Accueil = new FenetrePrincipale();
+		Accueil.setSize(700, 500);
+		}
+
+}
